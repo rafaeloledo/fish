637ca46650556ed03497a88998651742690c325f
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-03-22 05:44:20 -0300

    feat(fish): add anaconda3 to PATH

--- config.fish
+++ config.fish
@@ -5,4 +5,5 @@ if status is-interactive
   source /opt/asdf-vm/asdf.fish
   export EDITOR=/usr/sbin/nvim
   alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
+  export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
 end
