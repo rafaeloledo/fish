88a4283f1928858f7be4e3b48467d62292ee438b
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-03-24 06:43:13 -0300

    chore(fish): make paths separately

--- config.fish
+++ config.fish
@@ -2,9 +2,14 @@ if status is-interactive
   alias ll="exa --icons -la"
   alias cat="bat --style=auto --plain"
   alias vim="nvim"
-  source /opt/asdf-vm/asdf.fish
-  export EDITOR=/usr/sbin/nvim
   alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
   export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
-  source ~/.asdf/asdf.fish
 end
+
+if test $WSL_DISTRO_NAME = "Ubuntu"
+  source ~/.asdf/asdf.fish
+else
+  source /opt/asdf-vm/asdf.fish
+  export EDITOR=/usr/sbin/nvim
+end
+
