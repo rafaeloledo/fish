3d942074c89c9f848b6ce3fe37958c66de566a88
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-01 18:19:27 -0300

    fix(fish): adjust from recent removes

---
+++ config.fish
@@ -0,0 +1,28 @@
+set fish_greeting ""
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+
+if status is-interactive
+  alias ll="exa --icons -la"
+  alias cat="bat --style=auto --plain"
+  alias vim="nvim"
+  alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
+  export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
+end
+
+if test $WSL_DISTRO_NAME = "Ubuntu"
+  source ~/.asdf/asdf.fish
+else
+  source /opt/asdf-vm/asdf.fish
+  export EDITOR=/usr/sbin/nvim
+  export PATH="/usr/lib/jvm/default/bin:$PATH"
+  export JAVA_HOME="/usr/lib/jvm/default"
+  export JDTLS_HOME="/usr/share/java/jdtls"
+end
+
