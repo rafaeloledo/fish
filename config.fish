7ac2a8a77ab228ff71eff89ef962b1239e6053ff
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-01 16:42:19 -0300

    feat(fish): add theme-related lines

--- config.fish
+++ config.fish
@@ -1,5 +1,13 @@
 set fish_greeting ""
 
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+
 if status is-interactive
   alias ll="exa --icons -la"
   alias cat="bat --style=auto --plain"
