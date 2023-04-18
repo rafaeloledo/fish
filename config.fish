bcdad34b068101d7d7b3b000bd7b548296ff1539
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-18 20:30:16 -0300

    fix(bat): resolve wsl distro conflict
    on ubuntu 'bat' is 'batcat'

--- config.fish
+++ config.fish
@@ -10,7 +10,6 @@ set -g theme_hostname always
 if status is-interactive
   alias g git
   alias ll="exa --icons -la"
-  alias cat="bat --style=auto --plain"
   alias vim="nvim"
   alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
   export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
@@ -18,7 +17,9 @@ end
 
 if test $WSL_DISTRO_NAME = "Ubuntu"
   source ~/.asdf/asdf.fish
+  alias cat="batcat --style=auto --plain"
 else
+  alias cat="bat --style=auto --plain"
   source /opt/asdf-vm/asdf.fish
   export EDITOR=/usr/sbin/nvim
   export PATH="/usr/lib/jvm/default/bin:$PATH"
