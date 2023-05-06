1357a7bac40c1b50a79448270c00e3150ea257ac
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-05-06 12:30:48 -0300

    fix(fish): resolve env variables conflict
    make both wsl and desktop linux work in the logic

--- config.fish
+++ config.fish
@@ -7,30 +7,35 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
+# my env variables
+set -gx DISTRO UBUNTU
+# set -gx DISTRO ARCH
+
 alias g git
 alias ll="exa --icons -laF"
 alias vim="nvim"
 alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
-alias distro="echo $WSL_DISTRO_NAME"
+alias distro="echo DISTRO"
 
 export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
 
 source ~/.config/fish/functions/upgraded_cd.fish
 
-
-if test $WSL_DISTRO_NAME = "Ubuntu"
-  source ~/.asdf/asdf.fish
-  alias cat="batcat --style=auto --plain"
-  alias brave="brave-browser"
-  alias upall="sudo apt update -y && sudo apt upgrade -y"
-  alias pgadmin4="/usr/pgadmin4/bin/pgadmin4"
-else if test $WSL_DISTRO_NAME = "Arch"
-  alias upall="sudo pacman -Syu --noconfirm"
-  alias cat="bat --style=auto --plain"
-  source /opt/asdf-vm/asdf.fish
-  export EDITOR=/usr/sbin/nvim
-  export PATH="/usr/lib/jvm/default/bin:$PATH"
-  export JAVA_HOME="/usr/lib/jvm/default"
-  export JDTLS_HOME="/usr/share/java/jdtls"
+if test -n DISTRO
+  if test DISTRO = "UBUNTU"
+    source ~/.asdf/asdf.fish
+    alias cat="batcat --style=auto --plain"
+    alias brave="brave-browser"
+    alias upall="sudo apt update -y && sudo apt upgrade -y"
+    alias pgadmin4="/usr/pgadmin4/bin/pgadmin4"
+  else if test DISTRO = "ARCH"
+    alias upall="sudo pacman -Syu --noconfirm"
+    alias cat="bat --style=auto --plain"
+    source /opt/asdf-vm/asdf.fish
+    export EDITOR=/usr/sbin/nvim
+    export PATH="/usr/lib/jvm/default/bin:$PATH"
+    export JAVA_HOME="/usr/lib/jvm/default"
+    export JDTLS_HOME="/usr/share/java/jdtls"
+  end
 end
 
