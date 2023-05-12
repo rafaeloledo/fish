ac16f1c1df85fdc6ca642bcd1694620d79c1b338
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-05-11 22:23:45 -0300

    chore(fish): update config.fish
    add aliases for original ls
    
    add conditional exa
    
    add command -qv checking for vim, cat and upall
    
    add bin folders to PATH
    
    remove unnecessary code

--- config.fish
+++ config.fish
@@ -1,39 +1,53 @@
 set fish_greeting ""
 
 # theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
+# set -g theme_color_scheme terminal-dark
+# set -g fish_prompt_pwd_dir_length 1
+# set -g theme_display_user yes
+# set -g theme_hide_hostname no
+# set -g theme_hostname always
 
 # my env variables - uncomment depending on the current distro
 # set -Ux DISTRO UBUNTU
 set -Ux DISTRO ARCH
 set -gx TMUX_CONFIG "$HOME/.config/tmux/tmux.conf"
+set -gx EDITOR nvim
 
+# aliases
 alias g git
-alias ll="exa --icons -laF"
-alias vim="nvim"
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+
+command -qv nvim && alias vim nvim
+
+command -qv bat && alias cat "bat --style=auto --plain"
+command -qv batcat && alias cat "batcat --style=auto --plain"
+
+command -qv apt && alias upall "sudo apt update -y && sudo apt upgrade -y"
+command -qv pacman && alias upall "sudo pacman -Syu --noconfirm"
+
 alias ccache="sync; echo 3 > /proc/sys/vm/drop_caches"
 alias distro="echo DISTRO"
 alias cls="clear"
 
-export PATH="/home/rgnh55/anaconda3/bin:/home/rgnh55/.local/bin:$PATH"
-export PATH="/usr/pgadmin4/bin:$PATH"
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx ~/.local/bin $PATH
 
 source ~/.config/fish/functions/upgraded_cd.fish
 source ~/scripts/fish/gdrive.fish 2> /dev/null
 
 if test $DISTRO = "UBUNTU"
   source ~/.asdf/asdf.fish 2> /dev/null
-  alias cat="batcat --style=auto --plain"
-  alias upall="sudo apt update -y && sudo apt upgrade -y"
 else if test $DISTRO = "ARCH"
-  alias upall="sudo pacman -Syu --noconfirm"
-  alias cat="bat --style=auto --plain"
   source /opt/asdf-vm/asdf.fish 2> /dev/null
-  export EDITOR=/usr/sbin/nvim
   export PATH="/usr/lib/jvm/default/bin:$PATH"
   export JAVA_HOME="/usr/lib/jvm/default"
   export JDTLS_HOME="/usr/share/java/jdtls"
