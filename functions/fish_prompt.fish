cc89798ae97463820cc194cd9e15ca78c6a6e5ce
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-06-06 22:18:33 -0300

    chore(fish): update config

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,6 +2,10 @@ function fish_prompt -d "Write out the prompt"
     # This shows up as USER@HOST /home/user/ >, with the directory colored
     # $USER and $hostname are set by fish, so you can just use them
     # instead of using `whoami` and `hostname`
+    set dir (prompt_pwd)
+    set parts (string split "/" $dir)
+    set trunc_dir (string join "" $parts[-1])
+    set fstring (string join "  "   $trunc_dir)
     printf '%s%s%s 󰁕 '  \
-      (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+      (set_color $fish_color_cwd) "$fstring" (set_color normal)
 end
